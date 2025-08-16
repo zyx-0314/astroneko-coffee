d151816a02ec85ecb60fe9c706b07b7d618e96a7
zyx-0314-avalanche <ramirezian037@gmail.com>
2025-08-17 01:49:56 +0800

    feat: configure JPA repositories and update application properties for PostgreSQL

--- backend/src/main/java/coffee/astroneko/backend/BackendApplication.java
+++ backend/src/main/java/coffee/astroneko/backend/BackendApplication.java
@@ -2,12 +2,13 @@ package coffee.astroneko.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = "coffee.astroneko.backend.repository")
+@SpringBootApplication(scanBasePackages = "coffee.astroneko.backend")
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(BackendApplication.class, args);
+  }
 }
